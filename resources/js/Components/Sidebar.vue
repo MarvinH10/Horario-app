ecef4b448f97eb2b0bcee479e288327ecc74c0fe
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2025-02-15 16:04:35 -0500

    Logo agregado para el sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -30,14 +30,13 @@ const sidebarClasses = computed(() => ({
 <template>
     <div>
         <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden" @click="$emit('close')"></div>
-
         <aside
-            class="w-64 bg-gray-800 text-white h-screen fixed top-0 left-0 transition-transform duration-300 ease-in-out z-50"
+            class="w-64 bg-[#333333] text-white h-screen fixed top-0 left-0 transition-transform duration-300 ease-in-out z-50"
             :class="sidebarClasses">
-            <div class="p-4 flex justify-between items-center">
-                <Link :href="route('dashboard')" class="flex items-center">
-                <ApplicationMark class="h-9 w-auto" />
-                <h2 class="text-2xl font-semibold ml-2">Asistencias</h2>
+            <div class="flex justify-center items-center">
+                <Link :href="route('dashboard')" class="flex flex-col items-center text-center">
+                <ApplicationMark />
+                <h3 class="text-lg text-gray-300">Asistencias</h3>
                 </Link>
                 <button v-if="props.cellphonesize" class="ml-auto" @click="$emit('close')">
                     <ion-icon style="color: rgb(156 163 175 / var(--tw-text-opacity, 1));" name="lock-open-outline"
